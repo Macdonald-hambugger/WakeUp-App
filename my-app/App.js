b7b798891dd3de4f7c8532b24325668fc361ed17
정삼복 <68502584+3boku@users.noreply.github.com>
2024-02-18 10:46:53 +0900

    RN 테스트

---
+++ my-app/App.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { WebView } from "react-native-webview";
+import { StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
+export default function App() {
+    return (
+        <WebView
+            style={styles.container}
+            source={{ uri: "https://youtubejocoding.netlify.app" }}
+        />
+    );
+}
